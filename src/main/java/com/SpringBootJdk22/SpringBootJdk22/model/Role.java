0a70e0800d13528dbb680fa07b724182ad4f60f7
Luan <luanml0016@gmail.com>
2024-11-04 10:14:05 +0700

    update

--- src/main/java/com/SpringBootJdk22/SpringBootJdk22/model/Role.java
+++ src/main/java/com/SpringBootJdk22/SpringBootJdk22/model/Role.java
@@ -17,7 +17,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "role")
+@Table(name = "roles")
 public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/SpringBootJdk22/SpringBootJdk22/model/User.java
+++ src/main/java/com/SpringBootJdk22/SpringBootJdk22/model/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
